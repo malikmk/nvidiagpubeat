a0b3893ac9aae1f0b74352f0c8c4d62eccb5711b
Chetan Desh <chetan@bonsai.ai>
2016-11-10 20:32:54 -0800

    Replaced old owner 'zen-chetan' with new owner 'BonsaiAI' in import

--- main.go
+++ main.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"os"
-
 	"github.com/elastic/beats/libbeat/beat"
-
-	"github.com/zen-chetan/nvidiagpubeat/beater"
+	"github.com/BonsaiAI/nvidiagpubeat/beater"
 )
 
 func main() {
